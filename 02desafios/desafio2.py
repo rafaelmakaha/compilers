56e416ef5a7f45b86d105d79f4e4822b567e068f
Rafael Makaha <rafael.makaha@gmail.com>
2019-05-05 16:24:11 -0300

    Finishes 02Challenge on Python.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- 02desafios/desafio2.py
+++ 02desafios/desafio2.py
@@ -1,78 +1,13 @@
-'''
-    Aluno: Rafael Makaha Gomes Ferreira
-    Matrícula: 160142369
-
-    Desafio 2.
-    A sua tarefa é projetar um Autômato Finito
-    que aceita uma linguagem L com palavras pertencentes
-    ao alfabeto [A-Za-z\ ] tal que cada palavra w não
-    contenha letras maíusculas após uma minúscula.
-
-    Solução:
-        A solução consiste em um Autômato Finito contendo
-        9 estados.
-        O estado inicial fica em loop até chegar a letra 'm'.
-        Os estados 1 a 8 esperam a entrada da palavra maratona.
-        Caso não sejam lidas todas as letras da palavra esperada,
-        retorna-se para o estado inicial.
-        Caso seja lida a palavra 'maratona', o estado final 8
-        fica em loop até o final da leitura da string de entrada.
-
-        Sobre as entradas...
-        1. Fazer as entradas dos estados com os chars que esperamos.
-        2. Fazer as entradas dos estados com os
-            chars que não esperamos, mas chamar eles
-            de uma variável só, tipo '$'.
-
-        Quando estivermos verifiando a string alvo...
-        1. Verificar se o char atual é o esperado.
-        2. Fazer uma das duas possibilidades já salvas no dicionário/matriz.
-
-    O código feito em sala de aula foi adaptado para python.
-
-    Entrada.
-    9
-    53 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z
-    0 m 1
-    0 $ 0
-    1 a 2
-    1 $ 0
-    2 r 3
-    2 $ 0
-    3 a 4
-    3 $ 0
-    4 t 5
-    4 $ 0
-    5 o 6
-    5 $ 0
-    6 n 7
-    6 $ 0
-    7 a 8
-    7 $ 0
-    8 $ 8
-    8 $ 8
-    0
-    1 8
-    Minha alegria eh fazer AFDs
-'''
-
-
 def foo(atual, buf, bpos, tabtran, finais, alfabeto):
-    # Verifica se o bufer esta vazio
-    # Verifica se todas as letras lidas pertencem ao alfabeto
-    # Verifica se o último estado é um estado final
-    if len(buf)==0 and finais[atual] == 1:
+    if bpos == len(buf) and finais[atual] == 1:
         return True
-
-    # Verifica se existem proximos estados possiveis
-    elif len(tabtran[atual][buf[bpos]])== 0 or not(buf[bpos] in alfabeto):
+    elif bpos == len(buf) or len(tabtran[atual][buf[bpos]]) == 0 or not(buf[bpos] in alfabeto):
         return False
     else:
-        # Chama a funcao recusivamente para cada possibilidade
         for i in range(len(tabtran[atual][buf[bpos]])):
-            atual = tabtran[atual][buf[bpos]][i]
-            if foo(atual, buf, bpos+1, trabtran, finais):
+            if foo(tabtran[atual][buf[bpos]][i], buf, bpos+1, tabtran, finais, alfabeto):
                 return True
+     
 
 def main():
     #Leitura da quantidade de estados
@@ -81,26 +16,23 @@ def main():
     #Leitura do alfabeto e da quantidade de simbolos
     alfabeto = [x for x in input().split(' ')]
     simbolos = int(alfabeto.pop(0))
-    alfabeto.pop(-1)
-    alfabeto[-1] = ' '
 
     #Leitura da tabela de transição
     tabtran = {}
-    possibilidades = estados * simbolos
+    # possibilidades = estados * simbolos
     i = 0
     for i in range(estados):
         tabtran[i] = {}
-        for j in range(possibilidades):
+        for j in range(simbolos):
             #Leitura da posição atual, seu trigger
             #e o destino na tabela de transição
             st = input().split(' ')
-            print(st)
             atual = st[0]
             trig = st[1]
             if not int(st[2]):
                 prox = []
             else:
-                prox = st[3:]
+                prox = list(map(int,st[3:]))
 
             tabtran[i][trig] = prox
 
@@ -121,10 +53,7 @@ def main():
     buf = input()
     bpos = 0
 
-    x = foo(atual, buf, bpos, tabtran, finais, alfabeto)
-
-
-    if x:
+    if foo(atual, buf, bpos, tabtran, finais, alfabeto):
         print('Aceito')
     else:
         print('Rejeito')
