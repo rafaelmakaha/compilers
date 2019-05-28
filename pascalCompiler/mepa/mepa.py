5457da21929479d531a2020e2057a5a924433f51
Rafael Makaha <rafael.makaha@gmail.com>
2019-05-28 15:24:05 -0300

    Starts mepa implementation.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ pascalCompiler/mepa/mepa.py
@@ -0,0 +1,34 @@
+dic = {
+    'INPP': lambda ,
+    'CRCT': lambda ,
+    'SOMA': lambda ,
+    'MULT': lambda ,
+    'SUBT': lambda ,
+    'DIVI': lambda ,
+    'INVR': lambda ,
+    'NEGA': lambda ,
+    'CONJ': lambda ,
+    'DISJ': lambda ,
+    'CMME': lambda ,
+    'CMMA': lambda ,
+    'CMIG': lambda ,
+    'CMDG': lambda ,
+    'CMAG': lambda ,
+    'CMEG': lambda ,
+    'DSVF': lambda ,
+    'DSVS': lambda ,
+    'NADA': lambda ,
+    'AMEM': lambda ,
+    'DMEM': lambda ,
+    'CRVL': lambda ,
+    'ARMZ': lambda ,
+    'IMPR': lambda ,
+    'LEIT': lambda ,
+}
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
